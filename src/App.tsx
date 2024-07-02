43f02abb1a3faeb0c8b4ec2b8a24aff1f96b8db1
Ronaldo Barbosa <ronaldo.s.barbosa@outlook.com>
2024-07-02 16:36:22 -0300

    basic header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Auth from './Auth'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <Auth/>
+      <Home />
     </>
   )
 }
--- src/pages/Header/index.tsx
+++ src/pages/Header/index.tsx
@@ -1,21 +1,35 @@
 import user from "../../assets/user.png"
+import cash from "../../assets/cash.png"
+import menu from "../../assets/menu.png"
 
 export default function Header() {
-    return(
+    return (
         <>
-        <div className="flex flex-row w-ful justify-between bg-gradient-to-r from-70% from-green-600 to-white">
-            <div>Menu</div>
-            <div>HoofMoney</div>
-            <div className="flex flex-row">
-                <div>
-                <div>Ola, User</div>
-                <div>Seu Saldo:</div>
+            <div className="flex flex-row w-ful justify-between bg-gradient-to-r from-70% from-green-600 to-white">
+                <div><img src={menu} alt="" /></div>
+                <div className="w-screen absolute mt-2">
+                    <p className="text-center font-bold font text-2xl">
+
+                        HoofMoney
+                    </p>
                 </div>
-                <div>
-                    <img src={user} alt="" />
+                <div className="flex flex-row">
+                    <div className="flex flex-col items-center text-center">
+                        <div className="flex gap-2">
+                            <p>Ola,</p>
+                            <p>Ronaldo Barbosa</p>
+                        </div>
+                        <div className="flex flex-row gap-1 m-1">
+                            <img src={cash} alt="" />
+                            <p>Seu Saldo: R$</p>
+                            <p>1,500 </p>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={user} alt="" />
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     );
 };
