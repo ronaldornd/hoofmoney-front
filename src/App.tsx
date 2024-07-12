0c12e74d1af906a634e90977f2db03c579dbe3be
silvamelo510 <thyago.smm@gmail.com>
2024-07-11 22:38:40 -0300

    AppTsx com rotas atuais para pesquisar pela URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import Auth from './Auth'
-import Home from './pages/Home'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importe o Routes
+
+import Home from './pages/Home';
+import Forget from './Auth/Forget';
+import Login from './Auth/Login';
+import Register from './Auth/Register';
 
 function App() {
-
-  return (
-    <>
-      <Home />
-    </>
-  )
+    return (
+        <Router>
+            <Routes> {}
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forget" element={<Forget />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
