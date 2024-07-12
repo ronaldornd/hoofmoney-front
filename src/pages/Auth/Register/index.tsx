c2a7c97d8147ad27cdd8b3af23624fbc41b88239
ronaldornd <>
2024-07-12 17:00:30 -0300

    Add social media links to forget and register pages

--- src/pages/Auth/Register/index.tsx
+++ src/pages/Auth/Register/index.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
-import * as AlertDialog from '@radix-ui/react-alert-dialog';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
+import * as AlertDialog from '@radix-ui/react-alert-dialog';
+import { useState } from "react";
+import about from "../../../assets/about.png";
+import contact from "../../../assets/contact.png";
+import instagram from "../../../assets/instagram.png";
 import logo from "../../../assets/logo.png";
 export default function Register() {
     const [isPopupVisible, setIsPopupVisible] = useState(false);
@@ -21,9 +23,23 @@ export default function Register() {
     return (
         <>
             <div className="flex flex-row w-screen h-screen items-center">
-                <div className="flex flex-col gap-2 w-full h-full p-10">
-                    <img src={logo} alt="logo da empresa Hoof Money" className="m-10 mb-0" />
-                    <h1 className="font-semibold text-xl text-center w-full">“Gerenciando seu futuro financeiro”</h1>
+                <div className="flex flex-col gap-2 w-full h-full p-10 justify-center items-center">
+                    <img src={logo} alt="logo da empresa Hoof Money" className="w-96 h-96 m-10 mb-0" />
+                    <h1 className="font-semibold text-xl text-center w-full h-max">“Gerenciando seu futuro financeiro”</h1>
+                    <div className="flex flex-row w-full h-max gap-3 text-center items-center justify-center">
+                        <a className='flex flex-col w-max items-center h-max gap-1'>
+                            <img className='h-8 w-8' src={instagram} alt="Logo do Instagram" />
+                            <p>Siga-nos</p>
+                        </a>
+                        <a className='flex flex-col w-max items-center h-max gap-1'>
+                            <img className='h-8 w-8' src={contact} alt="Logo do Instagram" />
+                            <p>Contate-nos</p>
+                        </a>
+                        <a className='flex flex-col w-max items-center h-max gap-1'>
+                            <img className='h-8 w-8' src={about} alt="Logo do Instagram" />
+                            <p>Sobre nós</p>
+                        </a>
+                    </div>
                 </div>
                 <div className="flex flex-col gap-5 w-full h-max items-center">
                     <h1 className="font-semibold text-2xl">Criar Conta</h1>
